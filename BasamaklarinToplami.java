8e537df7f67d8749ecb9a775f1465a57615e0c8b
iskenderColak <60299811+iColak@users.noreply.github.com>
2022-01-27 09:27:46 +0300

    Add files via upload

--- BasamaklarinToplami.java
+++ BasamaklarinToplami.java
@@ -4,24 +4,15 @@ import java.util.Scanner;
 
 public class BasamaklarinToplami {
     public static void main(String[] args) {
-        int sayi, tempNumber, bolum=0, toplam=0, basamakSayisi=0;
+        int sayi, toplam=0;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Bir sayı giriniz: ");
         sayi = scanner.nextInt();
-        tempNumber = sayi;
-        do{
-            bolum=tempNumber/10;
-            tempNumber = bolum;
-            basamakSayisi++;
-        }while (bolum!=0);
-
-        int rakam;
-        for(int i=1; i<=basamakSayisi; i++){
-            rakam = sayi%10;
+        while(sayi!=0){
+            int rakam = sayi % 10;
             toplam += rakam;
-            sayi/=10;
+            sayi /=10;
         }
-        System.out.println("Girilen sayının basamaklarının sayı değeri toplamı: " +toplam);
-
+        System.out.println(toplam);
     }
 }
